ae629365425b07978bea8542839a763e4d741090
Oujiii <34745265+Oujiii@users.noreply.github.com>
2021-05-08 19:57:45 -0300

    Update first.sh
    Git removal, should be installed before cloning the repo

--- first.sh
+++ first.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Update repo and install packages
-apt update && apt install curl htop screen git sudo ufw -y
+apt update && apt install curl htop screen sudo ufw -y
 
 echo "All packages were installed"
