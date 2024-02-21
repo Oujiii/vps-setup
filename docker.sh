d98bc8146dba9dcc0bda8557fa49cbc7cda23416
Oujiii <34745265+Oujiii@users.noreply.github.com>
2024-02-21 15:43:14 -0300

    Update docker.sh
    Update docker compose to v2

--- docker.sh
+++ docker.sh
@@ -9,9 +9,10 @@ sh get-docker.sh
 echo "Docker was installed"
 
 #Get the script install from Docker website and execute it 
-curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+
 
 echo "Docker-compose was installed"
 
